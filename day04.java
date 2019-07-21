1e138920c1a44ac999db111fdbca784e1488a669
Ipshita Ghosh <iuippi3@gmail.com>
2019-07-21 22:11:01 +0530

    Rename day4.java to day04.java

---
+++ day04.java
@@ -0,0 +1,42 @@
+/* Read input from STDIN. Print your output to STDOUT
+Decide yourself with conditional statement (100 Marks)
+For this challenge, you need to read a integer value(default name - age) from stdin, store it in a variable and then compare that value with the conditions given below - 
+    - if age is less than 10, then print 'I am happy as having no responsibilities.' to the stdout. 
+    - if age is equal to or greater than 10 but less than 18, then print 'I am still happy but starts feeling pressure of life.' to the stdout. 
+    - if age is equal to or greater than 18, then print 'I am very much happy as i handled the pressure very well.' to the stdout.  
+
+Input Format
+A single line to be taken as input and save it into a variable of your choice(Default name - age). 
+
+Constraints
+1 < = age < = 100 
+
+Output Format
+Print the sentence according to the value of the integer which you had taken as an input. 
+
+Sample TestCase 1
+Input
+8
+Output
+I am happy as having no responsibilities.*/
+
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+   public static void main(String args[] ) throws Exception {
+    
+	Scanner scan = new Scanner(System.in);
+   int age;
+   age =  scan.nextInt();
+   if(age >= 1 && age <= 100) 
+   {
+   if (age<10)
+   System.out.print("I am happy as having no responsibilities.");
+   else if(age>=10 && age <19)
+   System.out.print("I am still happy but starts feeling pressure of life.");
+   else if(age>=18)
+   System.out.print("I am very much happy as i handled the pressure very well.");
+   }
+
+   }
+}
